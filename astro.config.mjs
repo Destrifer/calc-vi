0caf4dfcf34afd2362a38b5b19477d6d08cb22db
Destrifer <destrifer@yandex.ru>
2025-02-02 20:18:14 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [vue()],
+  output: "server",
+  integrations: [vue()],
+  adapter: netlify(),
 });
